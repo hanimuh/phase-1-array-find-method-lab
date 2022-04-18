a1956b40212ca1e1de67756b5d3cfd574a6367de
Hani <md.hani@hotmail.com>
2022-04-18 07:23:16 +0300

    commit tree 3b957aee474580fef5aaaa3794af3e34388f3cc6

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+
+
+// const res = record.find (superbowlWin);
+// function superbowlWin (record){
+//     if (record.result === 'W'){
+//         console.log (record.year);
+//     }
+// }
+    
+// superbowlWin (record)
+
+
+
+let res = record.find (function superbowlWin (Array){
+    if (Array.result === "W"){
+        return Array.year;
+    }
+});
+console.log (res)
