a30f4a6c0d9af5dae4a4b6f96e609a09ce5f8d5b
Hani <md.hani@hotmail.com>
2022-04-18 07:23:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+
+
+
+
+let res = record.find (function superbowlWin (Array){
+    if (Array.result === "W"){
+        return Array.year;
+    }
+});
+console.log (res)
